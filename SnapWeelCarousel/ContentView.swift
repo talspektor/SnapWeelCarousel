ba8c86464e37948cc603883fa59e1b9eec66df9d
Tal Spektor <talspektor@gmail.com>
2022-04-02 19:59:34 +0300

    Initial Commit

---
+++ SnapWeelCarousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnapWeelCarousel
+//
+//  Created by Tal talspektor on 02/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnapWeelCarousel/SnapWeelCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWeelCarouselApp.swift
+//  SnapWeelCarousel
+//
+//  Created by Tal talspektor on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnapWeelCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
